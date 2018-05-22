a5f2992604d42658c1c4e2cc03efd5888973420d
Hukanzen <qazxswed1934@gmail.com>
2018-05-22 15:59:58 +0900

    jsonファイルを読み出し成功

--- main.pl
+++ main.pl
@@ -5,19 +5,53 @@ use warnings;
 use Data::Dumper;
 
 # macだと，cpanいる
-use JSON;
+#use JSON;
 
 
 #require './my_func/Perl/my_csv.pm';
 require './my_func/Perl/my_package.pm';
 
+&main;
+
 sub main
+{
+	#&userdata;
+	
+	&tweetdata;
+}
+
+sub userdata
 {
 	my $mypack=my_package->new;
 	
-	my @data=$mypack->load_file('./data/js/user_details.js');
+	my $user_details=$mypack->load_file('./data/js/user_details.js');
 	
-	print Dumper @data;
+	print Dumper $user_details;
 }
 
-&main;
+sub tweetdata
+{
+	#my $mypack=my_package->new;
+	
+	my @files=glob "./data/js/tweets/*.js";
+	print Dumper @files;
+	
+	foreach my $tweetjs(@files){
+		my $user_details=load_file($tweetjs);
+		print $user_details;
+	}
+}
+
+sub load_file
+{
+	my ($file)=@_;
+	my $data="";
+
+	open(RF,$file);
+	for(my $i=0;<RF>;$i++){
+		$data.=$_;
+	}
+	close(RF);
+
+	return $data;
+}
