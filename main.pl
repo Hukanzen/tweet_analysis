5dd0d9fcbaeeaa7a65ac861e9f31fd42f7122832
i13302 <i13302@sr.kagawa-nct.ac.jp>
2018-05-24 02:09:50 +0900

    Unicodeが難しい．．．．
    $ perl main.pl
    69D830783000always305530933061308730463081306E3086304630722019703067306F7121304F30663000ALWAYS4E094E0176EE306E591565E52019643067305930883002$
    $ python3 main.py
    様へ　alwaysさんちょうめのゆうひ’70では無くて　ALWAYS三丁目の夕日’64ですよ。
    $

--- main.pl
+++ main.pl
@@ -4,8 +4,19 @@ use strict;
 use warnings;
 use Data::Dumper;
 
+use Encode;
+
+# libunicode-escape-perl
+use Unicode::Escape qw(escape unescape);
+
+use utf8;
+binmode(STDOUT, ":utf8");
+
+use feature qw/say/;
 # macだと，cpanいる
 use JSON;
+
+$Data::Dumper::Useperl = 1;
 
 
 #require './my_func/Perl/my_csv.pm';
@@ -15,16 +26,17 @@ use JSON;
 
 sub main
 {
-	&userdata;
+	#&userdata;
 	
 	#&tweetdata;
+	print encode_utf8("\u69D8\u3078\u3000always\u3055\u3093\u3061\u3087\u3046\u3081\u306E\u3086\u3046\u3072\u201970\u3067\u306F\u7121\u304F\u3066\u3000ALWAYS\u4E09\u4E01\u76EE\u306E\u5915\u65E5\u201964\u3067\u3059\u3088\u3002");
 }
 
 sub userdata
 {
 	my $user_details=&load_file('./data/js/user_details.js');
 	
-	print Dumper decode_json($user_details);
+	#print Dumper decode_json($user_details);
 }
 
 sub tweetdata
@@ -32,12 +44,34 @@ sub tweetdata
 	#my $mypack=my_package->new;
 	
 	my @files=glob "./data/js/tweets/*.js";
+
 	#print Dumper @files;
 	
 	foreach my $tweetjs(@files){
-		my $user_details=load_file($tweetjs);
+		# my $user_details=load_file($tweetjs);
+		my $month_tweet=load_json_file('./data/js/tweets/2012_01.1.js');
+		
+		
+		my %tojson=to_json($month_tweet);
+		#print Dumper %tojson;
+		#print Dumper %tojson->{text};
+		#my $unescaped=unescape($tojson)	;
+		
+		#print Dumper decode('utf8',encode('utf8',$unescaped));
+		#print Dumper decode_utf8($unescaped);
+		
+		#print Dumper $unescaped;
+		
+		
+			# my $unescaped=unescape($_);
+			# print  Dumper $unescaped;
+			#print Dumper $_;
+		
 		#print $user_details;
-		#	print Dumper decode_json($user_details);
+		
+		#print Dumper to_json($user_details);
+		#print Dumper from_json($user_details);
+		#say ($user_details);
 	}
 }
 
@@ -52,5 +86,23 @@ sub load_file
 	}
 	close(RF);
 
+	return $data;
+}
+
+sub load_json_file
+{
+	my ($file)=@_;
+	my $data;
+	
+	open(my $fh,$file);
+	
+	eval{ 
+		local $/ = undef;
+		my $json_txt = <$fh>;
+		close $fh;
+		$data = decode_json( $json_txt ); 
+	};
+	close(RF);
+	
 	return $data;
 }
